96061a0bded1e270dd0658e1874d5e5635b95a4f
Vladik <55589606+VladAleinikov@users.noreply.github.com>
2025-02-27 15:31:33 +0300

    add genre repository

---
+++ src/main/java/org/example/bookslibrary/model/Genre/GenreRepository.java
@@ -0,0 +1,12 @@
+package org.example.bookslibrary.model.Genre;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, String> {
+    List<Genre> findGenresByNameContaining(String name);
+}
